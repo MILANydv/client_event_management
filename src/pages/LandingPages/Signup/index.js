94b673fabee41e72c1ec0cdcf1653ac9adeeba33
Milan Yadav <ymilan593@gmail.com>
2022-07-10 02:04:04 +0545

    UI Improved SignIn and SignUp

---
+++ src/pages/LandingPages/Signup/index.js
@@ -0,0 +1,173 @@
+import axios from "axios";
+import { useState } from "react";
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import MuiLink from "@mui/material/Link";
+
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import GoogleIcon from "@mui/icons-material/Google";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKButton from "components/MKButton";
+import MKInput from "components/MKInput";
+import MKTypography from "components/MKTypography";
+
+// Material Kit 2 React example components
+import SimpleFooter from "examples/Footers/SimpleFooter";
+
+
+
+// Images
+import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+
+function SignUpBasic() {
+ 
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (e) => {
+    try {
+      e.preventDefault(); // stop the form from reloading the page
+      const data = {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      };
+      await axios.post("http://localhost:5000/users/api/register", data).then((res) => {
+        if (Response.data.success === true) {
+          alert("User registered successfully");
+        } else {
+          alert("User registration failed");
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <MKBox
+        position="absolute"
+        top={0}
+        left={0}
+        zIndex={1}
+        width="100%"
+        minHeight="100vh"
+        sx={{
+          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+     
+      <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
+        <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
+          <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
+            <Card>
+              <MKBox
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+                mx={2}
+                mt={-3}
+                p={2}
+                mb={1}
+                textAlign="center"
+              >
+                <MKTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+                  Sign Up
+                </MKTypography>
+                <Grid container spacing={3} justifyContent="center" sx={{ mt: 1, mb: 2 }}>
+                  <Grid item xs={2}>
+                    <MKTypography component={MuiLink} href="#" variant="body1" color="white">
+                      <FacebookIcon color="inherit" />
+                    </MKTypography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <MKTypography component={MuiLink} href="#" variant="body1" color="white">
+                      <GitHubIcon color="inherit" />
+                    </MKTypography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <MKTypography component={MuiLink} href="#" variant="body1" color="white">
+                      <GoogleIcon color="inherit" />
+                    </MKTypography>
+                  </Grid>
+                </Grid>
+              </MKBox>
+              <MKBox pt={4} pb={3} px={3}>
+                <MKBox component="form" role="form">
+                  <MKBox mb={2}>
+                    <MKInput type="name" label="Name" fullWidth 
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </MKBox>
+                  <MKBox mb={2}>
+                    <MKInput type="username" label="UserName" fullWidth 
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </MKBox>
+                  <MKBox mb={2}>
+                    <MKInput type="email" label="Email" fullWidth 
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </MKBox>
+                  <MKBox mb={2}>
+                    <MKInput type="password" label="Password" fullWidth 
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </MKBox>
+
+                  <MKBox mt={4} mb={1}>
+                    <MKButton variant="gradient" color="info" fullWidth
+                    onClick={registerUser}
+                    >
+                      sign Up
+                    </MKButton>
+                  </MKBox>
+                  <MKBox mt={3} mb={1} textAlign="center">
+                    <MKTypography variant="button" color="text">
+                      Have an account?{" "}
+                      <MKTypography
+                        component={Link}
+                        to="/sign-in"
+                        variant="button"
+                        color="info"
+                        fontWeight="medium"
+                        textGradient
+                      >
+                        Sign In
+                      </MKTypography>
+                    </MKTypography>
+                  </MKBox>
+                </MKBox>
+              </MKBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MKBox>
+      <MKBox width="100%" position="absolute" zIndex={2} bottom="1.625rem">
+        <SimpleFooter light />
+      </MKBox>
+    </>
+  );
+}
+
+export default SignUpBasic;
