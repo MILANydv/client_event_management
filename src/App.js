86c6848d4bdf2b46835a29ed5abf228c6f7244e5
Milan Yadav <ymilan593@gmail.com>
2022-07-21 17:14:47 +0545

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-import './App.css';
-import React from "react";
-import Body from "./components/home/Body";
-import Footer from "./components/home/Footer";
-import NavBar from "./components/home/Navbar";
+import { useEffect } from "react";
 
+// react-router components
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
+// @mui material components
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 
-function App() {
+// Material Kit 2 React themes
+import theme from "./assets/theme";
+import HomePage from "./pages/LandingPages/HomePage";
+
+// Material Kit 2 React routes
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import Author from "./pages/Protected/Author";
+import Dashboard from "./pages/Protected/Dashboard";
+import routes from "./routes";
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Body />
-        <Footer />
-      </BrowserRouter>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar />
+
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/author" element={<Author />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   );
 }
-
-export default App;
