fe9d03292014f11c429dd5283cebf03422b74377
Milan Yadav <ymilan593@gmail.com>
2022-07-25 11:54:36 +0545

    used Props and Components And Local Storage

--- src/components/NavBar/NavBar.js
+++
@@ -1,9 +0,0 @@
-import DefaultNavbar from "../../examples/Navbars/DefaultNavbar";
-import routes from "../../routes";
-
-
-function NavBar() {
-  return <DefaultNavbar routes={routes} sticky />;
-}
-
-export default NavBar;
